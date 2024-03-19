cd897e9f5b0bb7e4048f77518a2e9cf0d464de77
Kornel Miszczak <kornelmiszczak@gmail.com>
2024-03-19 14:44:35 +0100

    init

---
+++ TCATest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCATest
+//
+//  Created by Kornel Miszczak on 19/03/2024.
+//
+
+import SwiftUI
+import ComposableArchitecture 
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCATest/TCATestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATestApp.swift
+//  TCATest
+//
+//  Created by Kornel Miszczak on 19/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TCATestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
